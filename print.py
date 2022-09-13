83dfbbf062f5a652e3e3e33be695bb45dd0c5519
Brigitte Mendez <brigitte.mendez@alarcongroup.us>
2022-09-13 18:01:19 -0500

    add: enclosed in def

--- print.py
+++ print.py
@@ -1,13 +1,15 @@
-# Print to the text file
-file = open('log.txt', 'w')
-print('This is a sample print male', file = file)
-file.close()
+def find_word_in_print():
+    file = open('log.txt', 'w')
+    print('This is a sample print male', file = file)
+    file.close()
 
-file1 = open("log.txt","r+")
-str_file1 =  file1.read()
-str_file = str_file1.split()
-print(str_file)
-file1.close()
+    file1 = open("log.txt","r+")
+    str_file1 =  file1.read()
+    str_file = str_file1.split()
+    print(str_file)
+    file1.close()
 
-if 'male' in str_file:
-    print("success")
+    if 'male' in str_file:
+        print("success")
+
+find_word_in_print()
